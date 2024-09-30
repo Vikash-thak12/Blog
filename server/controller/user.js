80b34e54ebad1965c9614f91ce09400fec95ecd0
Vikash Thakur <thakvikash622@gmail.com>
2024-09-30 21:42:05 +0530

    Fixed: Signup Functionality

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,4 +1,5 @@
 import User from "../models/user-model.js";
+import bcrypt from "bcrypt"
 
 export const SignUp = async (req, res) => {
     try {
@@ -8,15 +9,31 @@ export const SignUp = async (req, res) => {
                 message: "Please fill all the details"
             })
         }
-        const response = await User.create({
+        const existUser = await User.findOne({ email })
+        if(existUser) {
+            return res.status(400).json({
+                message: "User Already Exists"
+            })
+        }
+        const hashed = await bcrypt.hash(password, 10)
+        const newUser = await User.create({
             name,
-            password,
+            password: hashed,
             email
         })
         return res.status(201).json({
-            message: "User Created Successfully"
+            message: "User Created Successfully",
+            User: {
+                id: newUser._id,
+                name: newUser.name, 
+                email: newUser.email
+                
+            }
         })
     } catch (error) {
         console.log("Error while doing signup", error)
+        return res.status(500).json({
+            message: "Internal Server Error"
+        })
     }
 }
