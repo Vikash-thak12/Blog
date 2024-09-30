d8698ab305bb4eb0162db6589ca75f12c60ab88f
Vikash Thakur <thakvikash622@gmail.com>
2024-09-30 22:10:47 +0530

    Implemented: AuthMiddleware

---
+++ server/auth/auth.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const authMiddleware = async (req, res, next) => {
+    // Get token from Authorization header
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    // Check if token is provided
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "Access denied. No token provided."
+        });
+    }
+
+    try {
+        // Verify the token
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        console.log("Decoded token:", decoded);
+
+        // Attach user information to the request object
+        req.user = decoded.id; // You can also include other relevant data from decoded if needed
+        next(); // Proceed to the next middleware or route handler
+    } catch (error) {
+        console.error("Error while authenticating", error);
+        return res.status(403).json({ // 403 for forbidden access
+            success: false,
+            message: "Invalid token."
+        });
+    }
+};
+
+export default authMiddleware;
