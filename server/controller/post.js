4585df7c2f170a0d9da567774747dbcd8acf8ca2
Vikash Thakur <thakvikash622@gmail.com>
2024-10-01 22:18:41 +0530

    Created Controller for Post

---
+++ server/controller/post.js
@@ -0,0 +1,19 @@
+export const createPost = async (req, res) => {
+
+}
+
+export const getAllPosts = async (req, res) => {
+
+}
+
+export const getPostById = async (req, res) => {
+
+}
+
+export const updatePost = async (req, res) => {
+
+}
+
+export const deletePost = async (req, res) => {
+    
+}
