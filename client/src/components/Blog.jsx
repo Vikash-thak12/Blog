6dbc64f01039b46d08eb401457db2de48216b0bd
Vikash Thakur <125829628+Vikash-thak12@users.noreply.github.com>
2024-10-08 23:37:40 +0530

    Update Blog.jsx

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -6,7 +6,7 @@ const Blog = () => {
             </div>
             <div className="p-2">
                 <h1>Bloggle Powerful Blog Builder</h1>
-                <p className="h-12 line-clamp-2 text-gray-400">RuffRuff Table of Contents helps you improve readability, effective search engine optimization, and understanding of user needs (In combination with a heat-map tool). Automatic table of contents generation and display using the headings in the content. It is the only app that can automatically generate a table of contents for products and collections.Please try it. You can check the actual table of contents from  in the upper left corner.</p>
+                <p className="h-12 line-clamp-2 text-gray-400">RuffRuff Table of Contents helps you improve readability, effective search engine optimization, and understanding of user needs (In combination with a heat-map tool). Automatic table of contents generation and display using the headings in the content. It can automatically generate a table of contents for products and collections.Please try it. You can check the actual table of contents from  in the upper left corner.</p>
                 <div className="">
                     <p>Author: Vikash Thakur</p>
                     <p>Created At: <span>12th Oct, 2024</span></p>
