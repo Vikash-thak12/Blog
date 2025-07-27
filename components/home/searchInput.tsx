97464da252bf63cd2d16603e141c706cd0b7ff06
Vikash Thakur <thakvikash622@gmail.com>
2025-07-27 21:00:40 +0530

    Fixed: SearchInput

--- components/home/searchInput.tsx
+++ components/home/searchInput.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import { Input } from '../ui/input'
+import { Search } from 'lucide-react'
 const SearchInput = () => {
   return (
-    <div>
-      SearchInput
-    </div>
+    <form action="">
+        <div className='relative'>
+            <Search className='absolute left-3 top-2.5 h-4 w-4' />
+            <Input type='text' name='search' placeholder='Search Articles' className='pl-10 w-48 focus-visible:ring-1' />
+        </div>
+    </form>
   )
 }
 
