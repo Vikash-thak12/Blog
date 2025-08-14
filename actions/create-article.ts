29ccb747364aa80a33ea894cb54a1bc0f630fb5e
Vikash Thakur <thakvikash622@gmail.com>
2025-08-14 15:20:55 +0530

    Setup: Cloudinary

--- actions/create-article.ts
+++ actions/create-article.ts
@@ -5,6 +5,17 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
 const articleSchema = z.object({
     // title: z.string(),
     title: z.string().min(2, { message: "Title must be at least 3 characters" }).max(100),
@@ -47,5 +58,82 @@ export const createArticle = async (prevState: CreateformArticleState, formData:
         }
     }
 
+    const existingUser = await prisma.user.findUnique({
+        where: {clerkUserId: userId}
+    })
+
+    if(!existingUser){
+        return {
+            errors: {
+                formErrors: ["User not found, Please register before creating article.."]
+            }
+        }
+    }
+
+    // cloudinay for image 
+    const imageFile = formData.get('featuredImage') as File | null; 
+    if(!imageFile || imageFile.name === "undefined"){
+        return {
+            errors: {
+                featuredImage:["Image file is required"]
+            }
+        }
+    }
+
+
+    const arrayBuffer = await imageFile.arrayBuffer(); 
+    const buffer = Buffer.from(arrayBuffer); 
+
+    const uploadResponse: UploadApiResponse | undefined = await new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+            {resource_type: 'auto'}, 
+            (error, result) => {
+                if(error){
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            }
+        )
+        uploadStream.end(buffer)
+    })
+
+    const imageUrl = uploadResponse?.secure_url; 
+    if(!imageUrl){
+        return {
+            errors: {
+                featuredImage:["Failed to upload imageUrl"]
+            }
+        }
+    }
+
+    try {
+        await prisma.articles.create({
+            data: {
+                title: result.data.title, 
+                category: result.data.category, 
+                content: result.data.content, 
+                featuredImage: imageUrl, 
+                authorId: existingUser?.id,
+            }
+        })
+    } catch (error: unknown) {
+        if(error instanceof Error){
+            return{
+                errors: {
+                    formErrors: [error.message]
+                }
+            }
+        } else {
+            return{
+                errors: {
+                    formErrors: ['Some internal server error']
+                }
+            }
+        }
+    }
+
+
+    revalidatePath("/dashboard")  // this will refresh this route to get the updated data
     redirect("/dashboard")
 }
